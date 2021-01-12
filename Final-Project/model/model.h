18c5f354cb869601646af6d613796536739d5a8b
theprowills <70465283+theprowills@users.noreply.github.com>
2021-01-12 14:42:15 +0700

    update fungsi Recipe
    menambahkan element "satuan", dan "total_ins"

--- Final-Project/model/model.h
+++ Final-Project/model/model.h
@@ -1,9 +1,12 @@
 struct recipe {
-    char name[50][100];
-    char description[50][255];
-    char ingredient[50][20][50];
-    char instruction[50][500];
-    int type[50];
+    char name        [50][100];
+    char description [50][255];
+    char ingredient  [50][ 20][ 50];
+    char satuan      [50][ 20][ 50];
+    int  qty         [50][ 20];
+    char instruction [50][ 20][500];
+    int  total_ins   [50];
+    int  type        [50];
 };
 struct bahan {
     char name[100][100];
